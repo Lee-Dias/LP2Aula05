28f9cbb00fed968db3f43967bb250ffdc93cdee5
lee-dias <leedias.dev@gmail.com>
2024-10-21 17:51:41 +0100

    exercicio 2 e 3

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insere um numero inteiro: ");
+            try{
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero inserido: {i}" );
+            }catch(Exception){
+
+            }
+        }
+    }
+}
